676eca9a3c4ecf1529b95388fe1e11050780ffba
Vince Knight <vincent.knight@gmail.com>
2022-08-30 15:40:32 +0100

    Update docstrings to follow google style.

--- main.py
+++ main.py
@@ -8,6 +8,16 @@ def create_manuscripts(number_of_manuscripts, seed=None):
     """
     Generate a hypothetical manuscript which
     corresponds to a number uniformly sampled from 1 - 10.
+
+    Args:
+        number_of_manuscripts : int
+            The number of manuscripts to be sampled
+        seed : int
+            The random seed for the number generator. Allows for reproducible sampling.
+
+    Returns:
+        array
+        A numpy array of integers corresponding to the manuscripts.
     """
     np.random.seed(seed)
     return np.random.randint(low=1, high=10 + 1, size=number_of_manuscripts)
@@ -17,6 +27,18 @@ def review_manuscripts(manuscripts, imprecision_error_sd, other_error_sd):
     """
     Given a collection of manuscripts, add the imprecision error
     and the other error as sampled from a normal distribution
+
+    Args:
+        manuscripts : array
+            A numpy array of integers corresponding to manuscripts.
+        imprecision_error_sd : float
+            The standard deviation corresponding to the imprecision error
+        other_error_sd : float
+            The standard deviation corresponding to the other error
+
+    Returns:
+        array
+        A numpy array of floats corresponding to the reviews of each manuscript.
     """
     number_of_manuscripts = len(manuscripts)
     imprecision_error = np.random.normal(
@@ -32,6 +54,16 @@ def is_above_threshold(
 ):
     """
     Return whether or not the manuscrupts pass the threshold
+
+    Args:
+        manuscripts : array
+            A numpy array of integers corresponding to manuscripts.
+        threshold : int
+            A numeric score corresponding to an acceptance threshold for a manuscript.
+
+    Returns:
+        array
+        A numpy array of booleans.
     """
     return manuscripts >= threshold
 
@@ -44,6 +76,20 @@ def get_average_review_scores(
 ):
     """
     Get average review scores over a number of reviewers.
+
+    Args:
+        manuscripts : array
+            A numpy array of integers corresponding to manuscripts.
+        number_of_reviews : int
+            The number of time to repeat the review process.
+        imprecision_error_sd : float
+            The standard deviation corresponding to the imprecision error
+        other_error_sd : float
+            The standard deviation corresponding to the other error
+
+    Returns:
+        array
+        A numpy array of floats corresponding to the average review of each manuscript.
     """
     average_review_scores = np.mean(
         [
@@ -69,6 +115,22 @@ def is_above_threshold_based_on_average(
     """
     Repeat the reviews and return booleans on
     wether or not to accept based on average score.
+
+    Args:
+        manuscripts : array
+            A numpy array of integers corresponding to manuscripts.
+        number_of_reviews : int
+            The number of time to repeat the review process.
+        threshold : int
+            A numeric score corresponding to an acceptance threshold for a manuscript.
+        imprecision_error_sd : float
+            The standard deviation corresponding to the imprecision error
+        other_error_sd : float
+            The standard deviation corresponding to the other error
+
+    Returns:
+        array
+        A numpy array of booleans.
     """
     average_review_scores = get_average_review_scores(
         manuscripts=manuscripts,
@@ -82,6 +144,18 @@ def is_above_threshold_based_on_average(
 def count_votes(reviews, threshold):
     """
     Given an iterable of iterables of reviews returns the number of votes for each paper.
+
+
+    Args:
+        reviews : array
+            A numpy array of floats corresponding to the average review of each manuscript.
+        threshold : int
+            A numeric score corresponding to an acceptance threshold for a manuscript.
+
+    Returns:
+        array
+        A numpy array of integers corresponding to the number of votes in favour of accepting a
+        paper.
     """
     number_of_votes = np.sum(
         [review >= threshold for review in reviews],
@@ -100,8 +174,24 @@ def is_above_threshold_based_on_vote(
     """
     Repeat the reviews and return booleans on
     wether or not to accept based on voting.
+
+    Args:
+        manuscripts : array
+            A numpy array of integers corresponding to manuscripts.
+        number_of_reviews : int
+            The number of time to repeat the review process.
+        threshold : int
+            A numeric score corresponding to an acceptance threshold for a manuscript.
+        imprecision_error_sd : float
+            The standard deviation corresponding to the imprecision error
+        other_error_sd : float
+            The standard deviation corresponding to the other error
+
+    Returns:
+        array
+        A numpy array of booleans indicating if the number of votes for a given paper
+        is above the majority.
     """
-    # TODO Modularise to separate votes
     reviews = (
         review_manuscripts(
             manuscripts=manuscripts,
@@ -124,6 +214,27 @@ def accuracy_of_process(
 ):
     """
     Return the accuracy of a given review process.
+
+    Repeat the reviews and return booleans on
+    wether or not to accept based on voting.
+
+    Args:
+        manuscripts : array
+            A numpy array of integers corresponding to manuscripts.
+        threshold : int
+            A numeric score corresponding to an acceptance threshold for a manuscript.
+        process : function
+            A review function
+        number_of_reviews : int
+            The number of time to repeat the review process.
+        imprecision_error_sd : float
+            The standard deviation corresponding to the imprecision error
+        other_error_sd : float
+            The standard deviation corresponding to the other error
+
+    Returns:
+        float
+        The proportion of papers that were accepted correctly or rejected correctly.
     """
     accurate_decisions = is_above_threshold(
         manuscripts=manuscripts,
